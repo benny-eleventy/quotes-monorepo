669908d298030f26fdff6ada93f21fe5356c6556
sagar <sgrmhdk51@gmail.com>
2023-05-31 10:14:01 +0530

    first commit

---
+++ apps/web-app/src/App.tsx
@@ -0,0 +1,12 @@
+import StyledDiv from "components";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<StyledDiv>Hello</StyledDiv>
+		</>
+	);
+}
+
+export default App;
---
+++ shared/components/index.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+	background-color: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+	padding: 20px;
+	margin-bottom: 20px;
+	width: 100px;
+	height: 100px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	color: black;
+`;
+
+export default StyledDiv;

42ccdd82f89d6c03cffaf8acbd3d8f988d777708
sagar <sgrmhdk51@gmail.com>
2023-06-02 10:42:47 +0530

    feat(testing): jest and vitest config set up

--- shared/state/vitest.config.ts
+++ shared/state/vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { defineProject } from "vitest/config";
 
-export default defineConfig({
+export default defineProject({
 	test: {
 		globals: true,
+		environment: "jsdom",
 	},
 });
